19c3f0ab412d2fca14733b6d26857c1281d660fc
umarzaibb <umarzzaib2@gmail.com>
2026-05-26 20:20:55 +0500

    Navigation to feed page on successful login added

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -38,6 +38,8 @@ export default function Login() {
           },()=>{
             alert('Login failed! PLease try again.')
           });
+
+          router.push('/feed');
         }
       }).catch(()=>{
       
